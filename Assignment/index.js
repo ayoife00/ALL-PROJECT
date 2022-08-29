a20e28a0fa5773ebec648576e9860cef3a76abd8
Olalere Kareem <olalereolatunbosun@gmail.com>
2022-08-29 10:29:01 -0700

    Update index.js
    create a function

--- Assignment/index.js
+++ Assignment/index.js
@@ -2,16 +2,4 @@
 
 //Create a function
 function result(myArr) {
-  //Use a conditional statement to check true or false
-  if (myArr[0] === myArr[myArr.length - 1]){
-    return true;
-  };
-  else {
-    return false;
-  }
-}
-
-//Print to the browser
-const newh1 = document.createElement('h1');
-newh1.innerHTML = result([20, 30, 14, 18, 17, 16, 20]);
-document.body.appendChild(newh1);
+ 
