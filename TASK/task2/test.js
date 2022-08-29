9c4a76400a1c3e766f1e5067de5d248e80b1878e
Olalere Kareem <olalereolatunbosun@gmail.com>
2022-08-29 10:12:43 -0700

    Add files via upload

---
+++ TASK/task2/test.js
@@ -0,0 +1,15 @@
+//WRITE AN ARRAY OF NUMBERS
+const numbers =[-5,-2,-6,0,-1]
+
+//DECLARE A VARIABLE TO CHECK FOR THE HIGHEST 
+let highest = 0;
+
+//Write a for loop
+for(let i=0; i<numbers.length; i++){
+//Use an if statement to check for highest number
+    if (numbers[i] > highest){
+        highest = numbers[i]
+    }
+} 
+//Use the alert command
+alert(`output: ${largest}`);
