76532439130d4ffd38b8e00e14ef75c9d08427d8
Olalere Kareem <olalereolatunbosun@gmail.com>
2022-08-29 10:30:51 -0700

    Update index.js
    An if conditional statement to check true or  false

--- Assignment/index.js
+++ Assignment/index.js
@@ -2,7 +2,15 @@
 
 //Create a function
 function result(myArr){
-
-
+ //Use a conditional statement to check true or false
+  if (myArr[0] === myArr[myArr.length - 1]){
+    return true;
+  };
+  else {
+    return false;
+  }
 }
+
+
+
  
